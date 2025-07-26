96ab02473cdfb5b97362e06f1b8f52e1d75659c5
Charmi Reddy P <23ra1a6620@kpritech.ac.in>
2025-07-26 19:57:12 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+if a > 5:
+    if b > 10:
+        for i in range(5):
+            if i % 2 == 0:
+                print("Even")
+            else:
+                print("Odd")
+
+def f():
+    x = 1
+    y = 2
+    z = x + y
+    return z
